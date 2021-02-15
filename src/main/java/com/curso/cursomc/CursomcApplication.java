28bc6c440ea2e736fc07b404fc08f7d59818c00d
litzenberger.bruno@gmail.com <bruno_l@microton.com.br>
2021-02-15 14:26:32 -0300

    Enviando imagem via endpoint

--- src/main/java/com/curso/cursomc/CursomcApplication.java
+++ src/main/java/com/curso/cursomc/CursomcApplication.java
@@ -1,17 +1,12 @@
 package com.curso.cursomc;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import com.curso.cursomc.services.S3Service;
-
 @SpringBootApplication
 public class CursomcApplication implements CommandLineRunner {
 
-	@Autowired
-	private S3Service s3Service;
 	
 	
 	public static void main(String[] args) {
@@ -20,7 +15,6 @@ public class CursomcApplication implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
-		s3Service.uploadFile("C:\\Users\\PcGamer\\Downloads\\people.jpg");
 		
 	}
 
--- src/main/java/com/curso/cursomc/services/AbstractEmailService.java
+++ src/main/java/com/curso/cursomc/services/AbstractEmailService.java
@@ -42,7 +42,7 @@ public abstract class AbstractEmailService implements EmailService {
 		sm.setSubject("Solicitação de nova senha");
 		sm.setSentDate(new Date(System.currentTimeMillis()));
 		sm.setText("Nova senha: " + newPass);
-		return sm;
+		return sm;	
 	}
 	
 	
